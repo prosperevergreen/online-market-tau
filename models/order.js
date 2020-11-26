d0e9f230e7cca3eefc61955a0d0a2a7858d808b8
Kaisa Sormunen <kaisa.sormunen@tuni.fi>
2020-11-26 21:36:37 +0200

    Implementing model for orders.

---
+++ models/order.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSubSchema = new Schema({
+   _id: {
+      type: String,
+      required: true
+   },
+   name : {
+      type: String,
+      required: true
+   },
+   price : {
+      type: Number,
+      required: true
+   },
+   description: { type: String },
+});
+
+const ProductSub = new mongoose.model('ProductSub', productSubSchema);
+
+const orderedItemSchema = new Schema({
+   product : {
+      type: productSubSchema,
+      required: true
+   },
+
+   quantity : {
+      type: Number,
+      required: true
+   }
+});
+
+const orderSchema = new Schema({
+  // Mongoose automatically creates _id (ObjectId) to all schemas
+  customerId : {
+     type: String,
+     required: true,
+  },
+  items : {
+     type: [orderedItemSchema],
+     required: true,
+     minlength: 1,
+  }
+});
+
+// Omit the version key when serialized to JSON
+orderSchema.set('toJSON', { virtuals: false, versionKey: false });
+
+
+
+const Order = new mongoose.model('Order', orderSchema);
+module.exports = Order;
