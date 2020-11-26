20d8ae1ec6ed1955e0578b885cf30bc50128ecae
prosperevergreen <prosperevergreen@gmail.com>
2020-11-26 21:51:23 +0200

    Task 11.1: Partial complete product model

---
+++ models/product.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+/**
+ * Custom validator for user e-mail format
+ *
+ * @param { string } url - product image url to be verified
+ * @returns { boolean } - return the validity of the url
+ */
+
+const urlValidator = (url) => {
+	return /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g.test(url);
+};
+
+const productSchema = new Schema({
+	// Mongoose automatically creates _id (ObjectId) to all schemas
+	// Product name
+	name: {
+		type: String,
+		required: true,
+	},
+	// Price of one product in Euros, without the Euro sign (€). Euros and cents are in the same float, with cents coming after the decimal point
+	price: {
+		type: Number,
+		required: true,
+		min: 0.01,
+		get: (val) => val.toFixed(2),
+	},
+	// Adding product images to the Web store API and pages is a Level 2 development grader substitute
+	image: {
+		type: String,
+		validate: urlValidator,
+	},
+	description: {
+		type: String,
+	},
+});
+
+
+// Omit the version key when serialized to JSON
+productSchema.set("toJSON", { virtuals: false, versionKey: false });
+
+const Product = new mongoose.model("Product", userSchema);
+module.exports = Product;
